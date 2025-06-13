409e3ad7aad8e1b282659c8a2ed1354a6d2884e1
Daniel James <danielt70@icloud.com>
2025-06-13 23:30:26 +0100

    Catch exceptions for invalid timezones

--- includes/functions.php
+++ includes/functions.php
@@ -30,6 +30,7 @@ final class functions {
 	 * Returns current localised time of a given timezone.
 	 * 
 	 * @since 1.0.0-b4
+	 * @since 1.0.0-rc Catch exception for invalid timezones.
 	 * 
 	 * @param  string $timezone A string representing a timezone selection.
 	 * 
@@ -37,12 +38,21 @@ final class functions {
 	 */
 	public function get_l10n_local_time( $timezone = 'UTC' ) {
 
-		/**
-		 * Required for \phpbb\datetime wrapper.
-		 * 
-		 * @link https://www.php.net/manual/en/class.datetimezone.php
-		 */
-		$dtz = new \DateTimeZone( $timezone );
+		try {
+
+			/**
+			 * Required for \phpbb\datetime wrapper.
+			 * 
+			 * @link https://www.php.net/manual/en/class.datetimezone.php
+			 */
+			$dtz = new \DateTimeZone( $timezone );
+
+		} catch ( \DateInvalidTimeZoneException $error ) {
+
+			// Always fallback to UTC.
+			$dtz = new \DateTimeZone( 'UTC' );
+
+		}
 
 		// phpBB wrapper class for php DateTime to localise timestamps.
 		$datetime = new \phpbb\datetime( $this->user, 'now', $dtz );
